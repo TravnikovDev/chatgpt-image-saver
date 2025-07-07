ac3707efed5aa7d9d7d906401e7a564b56709087
Roman Travnikov <roman@travnikov.dev>
2025-07-07 04:46:22 +0300

    Refactor image selection logic and remove unnecessary permissions from manifest

--- contentScript.js
+++ contentScript.js
@@ -1,8 +1,7 @@
 // Find all images under 'div.agent-turn' with src starting with 'https://files.oaiusercontent.com'
-document
-  .querySelectorAll(
-    'div.agent-turn img[src^="https://files.oaiusercontent.com"]'
-  )
+Array.from(document.querySelectorAll(".agent-turn"))
+  .map((el) => el.querySelector("img"))
+  .filter(Boolean)
   .forEach((img, index) => {
     chrome.runtime.sendMessage({
       action: "download",
